f89e3265d5a0c323ef0b96a7ea0a3368c039e7e0
Joseph Fischetti <fischettijw@gmail.com>
2020-12-23 13:43:32 -0500

    rotating but not turning

--- sketch.js
+++ sketch.js
@@ -4,10 +4,12 @@ let diam = (canvasSize - ballSize) / 2;
 let cX = (canvasSize / 2);
 let cY = (canvasSize / 2) + diam / 2;
 let clrs = ['green', 'red', 'magenta', 'brown', ' orange', 'yellow', 'cyan', 'blue'];
+let a = 0;
 
 function setup() {
     createCanvas(canvasSize, canvasSize);
     angleMode(DEGREES);
+    frameRate(2);
 }
 
 function draw() {
@@ -16,22 +18,25 @@ function draw() {
     stroke('black');
     strokeWeight(1);
     circle(canvasSize / 2, canvasSize / 2, canvasSize - ballSize);
-    drawCircle(90);
+
+
+    drawCircle(a);
     line(0, canvasSize / 2, canvasSize, canvasSize / 2);
     line(canvasSize / 2, 0, canvasSize / 2, canvasSize);
+
+    a += 30;
 }
 
 function drawCircle(angle) {
     push();
+
+    translate(width / 2, height / 2);
+    rotate(angle);
+
     fill(220);
     stroke('black');
     strokeWeight(1);
-
-    // translate(width / 2, height / 2);
-    // rotate(angle);
-
-    translate(width / 2, (height / 2) + diam / 2);
-    rotate(angle);
+    translate(0, diam / 2);
     circle(0, 0, diam);
 
     for (i = 0; i < clrs.length; i++) {
@@ -42,4 +47,5 @@ function drawCircle(angle) {
     }
 
     pop();
+
 }
