f366214f8478cbaac87e89cce968673d8536be01
Joseph Fischetti <fischettijw@gmail.com>
2020-12-23 10:14:03 -0500

    update

--- sketch.js
+++ sketch.js
@@ -3,6 +3,7 @@ let ballSize = 30;
 let diam = (canvasSize - ballSize) / 2;
 let cX = (canvasSize / 2);
 let cY = (canvasSize / 2) + diam / 2;
+let clrs = ['green', 'red', 'magenta', 'brown', ' orange', 'yellow', 'cyan', 'blue'];
 
 function setup() {
     createCanvas(canvasSize, canvasSize);
@@ -15,7 +16,7 @@ function draw() {
     stroke('black');
     strokeWeight(1);
     circle(canvasSize / 2, canvasSize / 2, canvasSize - ballSize);
-    drawCircle(22.5);
+    drawCircle(90);
     line(0, canvasSize / 2, canvasSize, canvasSize / 2);
     line(canvasSize / 2, 0, canvasSize / 2, canvasSize);
 }
@@ -23,54 +24,22 @@ function draw() {
 function drawCircle(angle) {
     push();
     fill(220);
-    stroke(240);
+    stroke('black');
     strokeWeight(1);
-    // translate(width * 0.25, height * 0.25);
-    translate(width / 2, (height / 2) + diam / 2);
+
     // translate(width / 2, height / 2);
+    // rotate(angle);
+
+    translate(width / 2, (height / 2) + diam / 2);
     rotate(angle);
     circle(0, 0, diam);
 
-    // translate(0, diam / 2);
-    // rotate(angle);
-    fill('green');
-    noStroke();
-    circle(0, diam / 2, ballSize);
+    for (i = 0; i < clrs.length; i++) {
+        fill(clrs[i]);
+        noStroke();
+        circle(0, diam / 2, ballSize);
+        rotate(360 / clrs.length);
+    }
 
-    fill('red');
-    noStroke();
-    rotate(45);
-    circle(0, diam / 2, ballSize);
-
-    fill('magenta');
-    noStroke();
-    rotate(45);
-    circle(0, diam / 2, ballSize);
-
-    fill('brown');
-    noStroke();
-    rotate(45);
-    circle(0, diam / 2, ballSize);
-
-    fill('magenta');
-    noStroke();
-    rotate(45);
-    circle(0, diam / 2, ballSize);
-
-    fill('yellow');
-    noStroke();
-    rotate(45);
-    circle(0, diam / 2, ballSize);
-
-    fill('cyan');
-    noStroke();
-    rotate(45);
-    circle(0, diam / 2, ballSize);
-
-
-    fill('blue');
-    noStroke();
-    rotate(45);
-    circle(0, diam / 2, ballSize);
     pop();
 }
