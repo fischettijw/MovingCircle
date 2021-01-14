284166145981626a12de3d42d0068f36e49041b6
Joseph Fischetti <fischettijw@gmail.com>
2021-01-14 15:34:23 -0500

    take out unneccessary rotates

--- rotatingCircle.js
+++ rotatingCircle.js
@@ -1,16 +1,19 @@
-let canvasSize = 900;
-let clrs = ['red', 'orange', 'yellow', 'chartreuse', 'green', 'springgreen',
-    'cyan', 'magenta', 'blue', 'hotpink', 'darkgray', 'coral',
-    'DarkSlateGray', 'Beige', 'NavajoWhite', 'DeepSkyBlue', 'LightCyan', 'LightSeaGreen'
-];
+let canvasSize = 480;
+let clrs = [
+    'red', 'DarkSlateGray', 'yellow', 'hotpink', '#00ff00', 'cyan',
+    'magenta', 'NavajoWhite', 'coral', 'lightblue', 'darkred', 'deepskyblue'
+]
+
+
 let ballSize = (3.14159 * (canvasSize)) / (4 * clrs.length);
 let rotCircleDiam = (canvasSize - ballSize) / 2;
-let incAngle = 0;
+let initialAngle = 0;
+let incAngle = initialAngle;
 
 function setup() {
     createCanvas(canvasSize, canvasSize).position(10, 10);
     angleMode(DEGREES);
-    frameRate(4);
+    frameRate(3);
 }
 
 function draw() {
@@ -18,13 +21,14 @@ function draw() {
     drawOuterCircle();
     drawRadialLines();
     drawRotatingCircle(incAngle);
-    incAngle += 360 / clrs.length / 2;
+    incAngle += (360 / clrs.length / 2);
+    // noLoop();
 }
 
 function drawOuterCircle() {
     fill(220);
     stroke('white');
-    strokeWeight(1);
+    strokeWeight(2);
     circle(canvasSize / 2, canvasSize / 2, canvasSize - ballSize);
 }
 
@@ -33,7 +37,7 @@ function drawRadialLines() {
         push();
         translate(width / 2, height / 2);
         rotate((360 / clrs.length / 2) * c);
-        stroke(clrs[c]);
+        stroke(color(clrs[c]));
         strokeWeight(3);
         line(0, -canvasSize / 2, 0, canvasSize / 2);
         pop();
@@ -49,11 +53,12 @@ function drawRotatingCircle(angle) {
     translate(0, rotCircleDiam / 2);
     fill(220, 0);
     stroke('white');
-    strokeWeight(1);
+    strokeWeight(2);
     circle(0, 0, rotCircleDiam);
+
     for (i = frameCount; i < frameCount + clrs.length; i++) {
         let ii = (i - 1) % clrs.length;
-        fill(clrs[ii]);
+        fill(color(clrs[ii]));
         noStroke();
         circle(0, rotCircleDiam / 2, ballSize);
         rotate(360 / clrs.length);
